0de12e671d551d6df79fcc9466005af601175559
Henrik Andersson <henrik@henrikandersson.org>
2019-03-15 14:16:13 +1000

    Add error message

--- Models/ErrorViewModel.cs
+++ Models/ErrorViewModel.cs
@@ -5,6 +5,7 @@ namespace hwapp.Models
     public class ErrorViewModel
     {
         public string RequestId { get; set; }
+        public string ErrorMessage { get; set; }
 
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
     }
